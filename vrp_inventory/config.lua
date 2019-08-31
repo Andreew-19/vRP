3ba329ece68e75b54724b089bea16f335e0b80de
Mihnea Banditul <47335726+MihneaBanditul@users.noreply.github.com>
2019-08-31 16:47:41 +0300

    Add files via upload

---
+++ vrp_inventory/config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+Config.OpenMenu = 303
+Config.AntiSpamCooldown = 2
+Config.Language = {
+    Title = "Inventar",
+    PleaseWait = "Vă rugăm să așteptați ...",
+    Error = "A fost o problemă.",
+    WarningTitle = "Avertizare",
+    WeaponNotEquipped = "Încă nu ați folosit arma.",
+    CannotBeUsed = "Acest articol nu poate fi utilizat din inventarul dvs.",
+    NotEnoughtSpace = "Persoana nu are loc în geantă",
+    NoNearby = "Nu există oameni în apropiere"
+}
