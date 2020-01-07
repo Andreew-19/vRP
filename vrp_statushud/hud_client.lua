a6576da98f87c54a1b20cbac59bf12d4928b3ee9
Mihnea Banditul <47335726+MihneaBanditul@users.noreply.github.com>
2020-01-07 17:13:03 +0200

    Update hud_client.lua
    Added Fuel

--- vrp_statushud/hud_client.lua
+++ vrp_statushud/hud_client.lua
@@ -8,6 +8,7 @@ Citizen.CreateThread(function()
         TriggerServerEvent("getFoameSete")
         SendNUIMessage({
             pauseMenu = IsPauseMenuActive(),
+            fuel = VehFuel(),
             health = GetEntityHealth(PlayerPedId())-100,
             armor = GetPedArmour(PlayerPedId()),
             food = food,
@@ -17,8 +18,24 @@ Citizen.CreateThread(function()
 end)
 
 
+
 RegisterNetEvent("returnFoameSete")
 AddEventHandler("returnFoameSete", function (Foame, Sete)
     food = Foame
     water = Sete
 end)
+
+
+
+
+function VehFuel()
+    local ped = PlayerPedId()
+    local vehicle = GetVehiclePedIsIn(ped, false) 
+    local fuelLevel = exports["LegacyFuel"]:GetFuel(vehicle)
+
+    if IsPedInAnyVehicle(PlayerPedId(), false) then 
+        return fuelLevel
+    else
+        return false
+    end
+end
