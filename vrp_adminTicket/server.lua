257ba92438a535f7ba05835615bbfa6f661d829e
Mihnea Banditul <47335726+MihneaBanditul@users.noreply.github.com>
2020-03-27 13:37:45 +0200

    Update server.lua

--- vrp_adminTicket/server.lua
+++ vrp_adminTicket/server.lua
@@ -5,28 +5,48 @@ vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP", "vrp_adminTicket")
 
 
+adminRequests = {}
+
 RegisterServerEvent('adminTicket')
 AddEventHandler('adminTicket', function(descriere)
   user_id = vRP.getUserId({source})
   player = vRP.getUserSource({user_id})
 
-  local raspuns = false
-
-  if vRP.hasPermission({user_id,"admin.tickets"})then
-  vRP.request({player,"ID ["..user_id.."] has a Problem: " ..descriere, 30, function(jucator,da)
-    if da then
-        if not raspuns then
-            vRPclient.notify(jucator,{"[ADMIN TICKET] ~g~Adminul ~r~" ..GetPlayerName(jucator).. " ~g~ti a acceptat Ticket-ul"})
-            vRPclient.getPosition(jucator, {}, function(x,y,z)
-            vRPclient.teleport(source,{x,y,z})
-        end)
-        raspuns = true
-    else
-        vRPclient.notify(source,{"[ADMIN TICKET] ~r~Acest Ticket a fost deja luat !"})
-     end
+  local answered = false
+  local players = {}
+  local users = vRP.getUsers({})
+  
+  for k,v in pairs(users) do
+    local player = vRP.getUserSource({tonumber(k)})
+    if vRP.isUserTrialHelper({k}) and source ~= nil then
+      table.insert(players,player)
     end
-  end})
-end
+  end
+
+  for k,v in pairs(players) do
+    usrID = vRP.getUserId({v})
+    if(adminRequests[user_id] == nil)then
+        vRP.request({v,"ID ["..user_id.."] has a Problem: " ..descriere, 30, function(v,ok)
+        adminRequests[user_id] = true
+        if ok then -- take the call
+          if not answered then
+            vRPclient.notify(player,{"An admin took your ticket."})
+            vRPclient.getPosition(player, {}, function(x,y,z)
+              vRPclient.teleport(v,{x,y,z})
+            end)
+
+            local theAdmin = vRP.getUserSource(usrID)
+            vRPclient.notify(v, {"[TICKET] ~r~Ticketul lui ~w~"..user_id.." ~r~luat de ~w~"..GetPlayerName(theAdmin)})
+            answered = true
+            adminRequests[user_id] = nil
+          end
+        else
+          vRPclient.notify(v,{"Ticket already taken."})
+        end
+      end})
+    else
+    end
+  end
 end)
 
 
